3a2e595dbeb883ceb55aa7fa2a66c4377ede9a81
Li <xiaoshan.a.li@accenture.com>
2017-07-27 17:27:23 +0800

    ver0.1

---
+++ src/pages/spa.module.ts
@@ -0,0 +1,19 @@
+// 当打包部署静态包时，需要加载本Module
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { AboutModule } from './about/about.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HomeModule,
+    UserModule,
+    AboutModule
+  ],
+})
+export class SPAModule { }
